f08e98c2370a32f5c1fa3e88b4af7c054be1d298
RamonaLuzia7 <ramonaluzia17@gmail.com>
2025-11-05 14:21:47 -0300

    commit

---
+++ src/main/java/br/com/senai/backend/estoque/models/Usuario.java
@@ -0,0 +1,10 @@
+package br.com.senai.backend.estoque.models;
+
+import java.time.LocalDate;
+public class Usuario {
+    private Integer id;
+    private String nome;
+    private String cargo; 
+    private LocalDate data_nascimento;
+    private String cpf;
+}
