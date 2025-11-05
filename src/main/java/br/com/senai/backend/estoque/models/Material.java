cdde3c6bbe55445cbc0dc2cbc0001361db74d84a
RamonaLuzia7 <ramonaluzia17@gmail.com>
2025-11-05 16:37:32 -0300

    commit - inclusão de modelagem: generation contrutor e get and set, import column/gene...value

--- src/main/java/br/com/senai/backend/estoque/models/Material.java
+++ src/main/java/br/com/senai/backend/estoque/models/Material.java
@@ -1,10 +1,70 @@
 package br.com.senai.backend.estoque.models;
 
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;import jakarta.persistence.Id;
 import java.time.LocalDate;
+
+@Entity
+@Table(name="material")
 public class Material {
+
+ @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+ @Column(name="id")
  private  Integer id;
+
+ @Column(name="nome")
  private  String nome;
- private  String marca; 
- private int ano_fabricacao;
+  
+ @Column(name="marca")
+  private  String marca; 
+  
+  @Column(name="ano_fabricacao")
+ private LocalDate ano_fabricacao;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getMarca() {
+    return marca;
+  }
+
+  public void setMarca(String marca) {
+    this.marca = marca;
+  }
+
+  public LocalDate getAno_fabricacao() {
+    return ano_fabricacao;
+  }
+
+  public void setAno_fabricacao(LocalDate ano_fabricacao) {
+    this.ano_fabricacao = ano_fabricacao;
+  }
+
+  public Material() {
+  }
+
+  public Material(Integer id, String nome, String marca, LocalDate ano_fabricacao) {
+    this.id = id;
+    this.nome = nome;
+    this.marca = marca;
+    this.ano_fabricacao = ano_fabricacao;
+  }
 
 }
--- src/main/java/br/com/senai/backend/estoque/models/Usuario.java
+++ src/main/java/br/com/senai/backend/estoque/models/Usuario.java
@@ -1,10 +1,81 @@
 package br.com.senai.backend.estoque.models;
 
 import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="usuario")
 public class Usuario {
+ @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+ @Column(name="id")
     private Integer id;
+   
+    @Column(name="nome")
     private String nome;
+    
+    @Column(name="cargo")
     private String cargo; 
+      
+    @Column(name="data_nascimento")
     private LocalDate data_nascimento;
+    
+    @Column(name="cpf")
     private String cpf;
+
+    public Usuario(Integer id, String nome, String cargo, LocalDate data_nascimento, String cpf) {
+        this.id = id;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.data_nascimento = data_nascimento;
+        this.cpf = cpf;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public LocalDate getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(LocalDate data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    
 }
